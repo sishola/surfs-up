f20f7c5f66e1af59d1766921f43496c8b66d48ec
sofiat <sofiatishola@gmail.com>
2019-08-17 01:32:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 
+###DB Connection
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
 app = Flask(__name__)
 
 hello_dict = {"Hello": "World!"}
@@ -14,18 +26,33 @@ hello_dict = {"Hello": "World!"}
 
 @app.route("/")
 def home():
-    f"Welcome to Surfs Up API!<br/>"
-    f"Available Routes:<br/>"
-    f"/api/v1.0/precipitation"
-    f"/api/v1.0/stations"
-    f"/api/v1.0/tobs"
-    f"/api/v1.0/<start>"
-    f"/api/v1.0/<start>/<end>"
+    return( 
+        f"Welcome to Surfs Up API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>" 
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt; <br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;"
+    )
+   
 
 
-@app.route("/normal")
-def normal():
-    return hello_dict
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    
+    prcp_data = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_data.append(prcp_dict)
+
+    return jsonify(prcp_data)
+
 
 
 @app.route("/jsonified")
