6e29ae1ab285140af650ad704d5e0f69b2111805
sofiat <sofiatishola@gmail.com>
2019-08-18 18:43:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 
-session = Session(engine)
 
 app = Flask(__name__)
 
@@ -48,6 +47,8 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
+    session = Session(engine)
+
     results = session.query(Measurement.date, Measurement.prcp).all()
     
     prcp_data = []
@@ -60,6 +61,8 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     
+    session = Session(engine)
+    
     results = session.query(Station.station).distinct().all()
     
     station_data = []
@@ -71,6 +74,8 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    
+    session = Session(engine)
     
     maxDate = session.query( func.max(Measurement.date).label('maxdate')).all()
     maxDate = list(np.ravel(maxDate))
@@ -86,6 +91,37 @@ def tobs():
         tobs_data.append([date,tobs])
 
     return jsonify(tobs_data)
+
+
+@app.route("/api/v1.0/<start_date>")
+def start_only(start_date):
+    
+    session = Session(engine)
+    
+    temp_tuple = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+
+    trip_temp = [list(np.ravel(temp_tuple))[0], list(np.ravel(temp_tuple))[1], list(np.ravel(temp_tuple))[2]]
+
+    trip_temp_list = [{'Min temperature': trip_temp[0]}, {'Avg temperature': trip_temp[1]}, {'Max temperature': trip_temp[2]}]
+        
+    return jsonify(trip_temp_list)
+
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end(start_date, end_date):
+    
+    session = Session(engine)
+    
+    temp_tuple = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+    trip_temp = [list(np.ravel(temp_tuple))[0], list(np.ravel(temp_tuple))[1], list(np.ravel(temp_tuple))[2]]
+
+   
+    trip_temp_list = [{'Min temperature': trip_temp[0]}, {'Avg temperature': trip_temp[1]}, {'Max temperature': trip_temp[2]}]
+        
+    #print(trip_temp_list)
+    #return jsonify({'a':start_date})
+    return jsonify(trip_temp_list)
 
 
 
